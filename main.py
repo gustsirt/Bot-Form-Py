3c0f58b522c77444f88da6ad3594e522eccbc330
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-29 16:11:57 -0300

    ➕ feat (prueb): lectura inicial de sheets

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.config.env import Config
 
+from app.pkg.services.google_client import get_google_sheets_client
+
 app = FastAPI()
 
 @app.get("/")
@@ -10,3 +12,14 @@ async def root():
       "PORT: ": Config.PORT,
       "sheets_id: ": Config.GOOGLE_SHEETS_ID
     }
+
+@app.get("/sheet-data")
+async def read_sheet():
+  sheet = get_google_sheets_client()
+  result = sheet.values().get(
+      spreadsheetId=Config.GOOGLE_SHEETS_ID,
+      range=Config.CONFIG_SHEET
+    ).execute()
+  
+  values = result.get("values", [])
+  return {"rows": values}
