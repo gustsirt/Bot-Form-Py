864a2a52a595f435ad6e67a091ea2e4e1ca90ca0
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-29 16:27:09 -0300

    ✨ feat (service): google sheet service

---
+++ app/pkg/services/sheet_service.py
@@ -0,0 +1,39 @@
+from google_client import get_google_sheets_client
+from app.config.env import Config
+
+# ✍️ Agrega una fila al Sheet en modo append.
+def add_to_sheet(data, range_=None, spreadsheet_id=None):
+  spreadsheet_id = spreadsheet_id or Config.GOOGLE_SHEETS_ID
+  range_ = range_ or Config.ANSWERS_SHEET
+
+  sheets = get_google_sheets_client()
+  
+  try:
+      sheets.values().append(
+          spreadsheetId=spreadsheet_id,
+          range=range_,
+          valueInputOption='RAW',
+          insertDataOption='INSERT_ROWS',
+          body={
+              "values": [data]
+          }
+      ).execute()
+      return "✅ Datos correctamente agregados"
+  except Exception as e:
+      print("❌ Error en add_to_sheet:", e)
+      raise e
+      
+# 📖 Lee datos desde el rango indicado.
+def get_from_sheet(range_, spreadsheet_id=None):
+  spreadsheet_id = spreadsheet_id or Config.GOOGLE_SHEETS_ID
+  sheets = get_google_sheets_client()
+  
+  try:
+      result = sheets.values().get(
+          spreadsheetId=spreadsheet_id,
+          range=range_
+      ).execute()
+      return result.get("values", [])
+  except Exception as e:
+      print("❌ Error en get_from_sheet:", e)
+      raise e
