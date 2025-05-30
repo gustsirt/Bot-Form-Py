b9e8d976ed6c9f42762ec3a384486ab539e8d5e8
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-30 15:55:10 -0300

    ➕ feat (config): router

---
+++ app/modules/routes.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter
+from app.config.env import Config
+from app.pkg.services.google_client import get_google_sheets_client
+from app.pkg.services.sheet_service import get_from_sheet, add_to_sheet
+
+router = APIRouter()
+
+@router.get("/")
+async def root(): 
+    return {
+        "message": "Hello World ✨",
+        "PORT": Config.PORT,
+        "sheets_id": Config.GOOGLE_SHEETS_ID
+    }
+
+@router.get("/sheet-data")
+async def read_sheet_raw():
+    sheet = get_google_sheets_client()
+    result = sheet.values().get(
+        spreadsheetId=Config.GOOGLE_SHEETS_ID,
+        range=Config.CONFIG_SHEET
+    ).execute()
+    values = result.get("values", [])
+    return {"rows": values}
+
+@router.get("/sheet")
+async def read_sheet():
+    values = get_from_sheet("TPREGUNTAS")
+    return {"data": values}
+
+@router.post("/sheet")
+async def write_sheet():
+    new_row = ["Juan", "Pérez", "juan@mail.com"]
+    message = add_to_sheet(new_row)
+    return {"message": message}
--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 from fastapi import FastAPI
 from app.config.env import Config
-
-from app.pkg.services.google_client import get_google_sheets_client
-from app.pkg.services.sheet_service import get_from_sheet, add_to_sheet
+from app.modules.routes import router
 
 app = FastAPI()
 
-@app.get("/")
-async def root(): 
-  return {
-      "message": "Hello World ✨",
-      "PORT: ": Config.PORT,
-      "sheets_id: ": Config.GOOGLE_SHEETS_ID
-    }
-
-@app.get("/sheet-data")
-async def read_sheet():
-  sheet = get_google_sheets_client()
-  result = sheet.values().get(
-      spreadsheetId=Config.GOOGLE_SHEETS_ID,
-      range=Config.CONFIG_SHEET
-    ).execute()
-  
-  values = result.get("values", [])
-  return {"rows": values}
-
-
-@app.get("/sheet")
-async def read_sheet():
-    values = get_from_sheet("TPREGUNTAS")  # o tu Config.CONFIG_SHEET
-    return {"data": values}
-
-@app.post("/sheet")
-async def write_sheet():
-    new_row = ["Juan", "Pérez", "juan@mail.com"]  # simulado
-    message = add_to_sheet(new_row)
-    return {"message": message}
+app.include_router(router)
