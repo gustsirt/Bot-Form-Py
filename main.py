23d2dc7359a53ae690a4c6bcd8e1e964228309ed
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-05-30 16:14:25 -0300

    ⬆️ update (main)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+# main.py
+
 from fastapi import FastAPI
 from app.config.env import Config
 from app.modules.routes import router
+from app.pkg.middleware.error_handler import ErrorHandlerMiddleware
 
+# App initialization ------------------------------
 app = FastAPI()
 
+# App Routes --------------------------------
 app.include_router(router)
+
+# App Middleware --------------------------------
+app.add_middleware(ErrorHandlerMiddleware)
